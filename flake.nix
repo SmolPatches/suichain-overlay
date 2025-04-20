d9d3fce3b742e63b01b3dc0cfb992833c42be689
mdnlss <rob73hall@gmail.com>
2025-04-20 12:16:01 -0400

    format: make the steps clearly seperated

--- flake.nix
+++ flake.nix
@@ -12,16 +12,14 @@
     flake-utils,
   }: let
     systems = with flake-utils.lib.system; [x86_64-linux aarch64-linux]; # binary platforms for sui
+    # overlays for sui
     overlays = {
       testnet = final: prev: {
         sui = self.outputs.packages.${final.system};
       };
     };
-  in
-    {
-      overlays = overlays;
-    }
-    // flake-utils.lib.eachSystem systems (system: let
+    # system outputs
+    systemOutputs = flake-utils.lib.eachSystem systems (system: let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [overlays.testnet];
@@ -31,4 +29,9 @@
       packages.sui-binary = pkgs.callPackage ./nix/package.nix {};
       packages.default = self.packages.${system}.sui-binary;
     });
+  in # merge the two into the actual outputs
+     {
+      overlays = overlays;
+    }
+    // systemOutputs; 
 }
