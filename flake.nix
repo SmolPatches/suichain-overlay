b0b585700fd7744fba2ce31cca1210da8ff3d87a
mdnlss <rob73hall@gmail.com>
2025-04-20 10:03:36 -0400

    flake-utils + change channel

--- flake.nix
+++ flake.nix
@@ -2,22 +2,25 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
   }: let
-    systems = ["x86_64-linux" "aarch64-linux"];
-    pkgs = nixpkgs.legacyPackages."x86_64-linux";
-  in {
-    formatter."x86_64-linux" = pkgs.alejandra;
-    packages.x86_64-linux.sui-binary = pkgs.callPackage ./nix/package.nix {};
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.sui-binary;
-    overlays.default = final: prev: {
-      sui = self.outputs.packages.${prev.system};
-    };
-  };
+    systems = with flake-utils.lib.system; [x86_64-linux aarch64-linux]; # binary platforms for sui
+  in
+    flake-utils.lib.eachSystem systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+      packages.sui-binary = pkgs.callPackage ./nix/package.nix {};
+      packages.default = self.packages.${system}.sui-binary;
+      overlays.default = final: prev: {
+        sui = self.outputs.packages.${prev.system};
+      };
+    });
 }
